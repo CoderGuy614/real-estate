125b44fc69bc4531a961af9b376d47f803adedb9
CoderGuy614 <jlutz.110@gmail.com>
2020-07-19 16:40:54 +0800

    Adjust CSS Grid on Contact Page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>THIS IS THE CONTACT PAGE</div>;
+  return (
+    <div className="contact">
+      <div className="contact-info">
+        <h5>Put Contact Info here</h5>
+        <div className="contact-row">Row1</div>
+        <div className="contact-row">Row2</div>
+      </div>
+      <div className="realtor-info">Put Realtor info here</div>
+    </div>
+  );
 };
 
 export default Contact;
