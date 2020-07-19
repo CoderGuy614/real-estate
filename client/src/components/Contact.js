9f44ab7cc6c1c2bf9eb2f650a34a85bc5062ce90
CoderGuy614 <jlutz.110@gmail.com>
2020-07-19 17:46:11 +0800

    Continue with grid layout exercises

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="contact">
-      <div className="contact-info">
-        <h5>Put Contact Info here</h5>
-        <div className="contact-row">Row1</div>
-        <div className="contact-row">Row2</div>
-      </div>
-      <div className="realtor-info">Put Realtor info here</div>
+    <div className="contact-container">
+      <div className="contact-item a">A</div>
+      <div className="contact-item b">B</div>
+      <div className="contact-item c">C</div>
+      <div className="contact-item d">D</div>
+      <div className="contact-item e">E</div>
+      <div className="contact-item f">F</div>
     </div>
   );
 };
